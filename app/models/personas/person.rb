2b141f4929c40ceba132a680b6c589d663014b02
Olvap <oldani.pablo@gmail.com>
2011-11-04 09:51:40 -0300

    ADD: parper_trail

--- app/models/personas/person.rb
+++ app/models/personas/person.rb
@@ -1,14 +1,23 @@
 module Personas
+
   class Person < ActiveRecord::Base
 
-    has_paper_trail
+    include Rails.application.routes.url_helpers # neeeded for _path helpers to work in models
+
+    def admin_permalink
+      admin_person_path(self)
+    end
+
     # FIX: cuando se solucione el problema de rails.
     def self.table_name
       "personas_people"
     end
+
     has_many :addresses
     belongs_to :civil
     belongs_to :sexo
     belongs_to :tdoc
+
   end
+
 end
