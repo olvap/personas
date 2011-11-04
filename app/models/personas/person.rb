b912ca2977abcaf9fb9ec0abafa2eb1a33615abb
Olvap <oldani.pablo@gmail.com>
2011-11-04 10:03:44 -0300

    ADD: paper_trail

--- app/models/personas/person.rb
+++ app/models/personas/person.rb
@@ -4,6 +4,8 @@ module Personas
 
     include Rails.application.routes.url_helpers # neeeded for _path helpers to work in models
 
+    has_paper_trail
+
     def admin_permalink
       admin_person_path(self)
     end
